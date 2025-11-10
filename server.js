ca4b874b8b9dadb3157843547d4f5ec881dc3a29
akash-maurya123 <akashmaurya7934@gmail.com>
2025-11-10 21:20:13 +0530

    some code update

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 // connect MongoDB
